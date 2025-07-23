502931a4013e07376473f410654510ce4f67b2ad
ARAVINTH <aravinthn7800@gmail.com>
2025-07-23 14:48:26 +0530

    Add change url

--- Products/urls.py
+++ Products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/',Home_page,name='home_page'),
+    path('',Home_page,name='home_page'),
     path('product/',Product_list,name='product'),
     path('search/',search_view,name='search_page'),
     path('product_view/<int:pk>/',Product_view,name='product_view'),
